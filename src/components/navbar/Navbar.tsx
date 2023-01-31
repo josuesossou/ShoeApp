8eb29c587a3da6e7fed413d0cf255bffb8a8f1b1
Josue Kokou Sossou <jsossou@terpmail.umd.edu>
2023-01-31 09:50:39 -0500

    fix navbar middle to be centered correctly and adjusting the sizes of the products

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,10 +3,11 @@ import styles from './Navbar.module.scss'
 import Logo from '../../assets/images/logo/Logo.png'
 import NameLogo from '../../assets/images/logo/Name-logo.png'
 import Link from 'next/link';
-import { Badge, Button, Grid, List } from '@mui/material';
+import { Badge, Button, List } from '@mui/material';
 import { navLinks } from '../../data/navLinks';
 import ListNavItem from './ListNavItem';
 import { useState } from 'react';
+import Grid from '@mui/material/Unstable_Grid2';
 
 
 
@@ -23,29 +24,27 @@ export default function Navbar() {
     return (
         <nav className={styles.nav}>
             <Grid container alignItems='center' justifyContent='space-around'>
+      
                 <Link href='/'>
                     <Image alt="Autrion" src={NameLogo} width={110} />
                 </Link>
-                
                 <Link href='/'>
                     <Image alt='' src={Logo} width={50} height={50}  />
                 </Link>
-                
-                <Grid item>
-                    <Grid container justifyContent='space-around' gap={2}>
-                        <Badge badgeContent={4} color="primary">
-                            <Button onClick={openNav} sx={{ width: '5em' }}>
-                                {isOpen? 'Close' : 'Menu'}
-                            </Button>
-                        </Badge>
-                        <Link href='/products'>
-                            <Button>
-                                Shop All
-                            </Button>
-                        </Link>
-                    </Grid>
-                </Grid>
+
+                <Badge badgeContent={4} color="primary">
+                    <Button onClick={openNav} sx={{ width: '5em' }}>
+                        {isOpen? 'Close' : 'Menu'}
+                    </Button>
+                </Badge>
+
             </Grid>
+            
+            <Link href='/products' className={styles.shop_all}>
+                <Button variant='outlined'>
+                    Shop All
+                </Button>
+            </Link>
 
             <List className={styles.list}>
                 {navLinks.map(navItem => (
--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -1,18 +1,21 @@
 import styles from './Products.module.scss'
+import Image from 'next/image';
 import { Card, CardContent, CardMedia, Typography } from "@mui/material";
 import { lenghtToArray } from "../../helpers/helpers";
+import shoe9 from '../../assets/images/shoe9.png'
 
 export default function ProductsComp() {
     return (
         <section className={styles.products_section}>
             {lenghtToArray(9).map(i => (
                 <Card elevation={0}>
-                    <CardMedia
-                        className={styles.cardMedia}
-                        image="http://localhost:1337/uploads/thumbnail_Test_main_solid_648307a9c0.jpeg"
-                        title="green iguana"
-                        
-                    />
+                    <div>
+                        <Image
+                            alt='img'
+                            src={shoe9}
+                        />
+                    </div>
+
                     <CardContent>
                         <small>Infinity</small>
                         <p>$160</p>
